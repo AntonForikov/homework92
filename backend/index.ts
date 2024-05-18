15ceb6201723a5e9859075626819713aec9d57cd
Anton Forikov <tohabass@gmail.com>
2024-05-18 15:22:34 +0600

    backend crete user model routes and logic for it

--- backend/index.ts
+++ backend/index.ts
@@ -2,15 +2,20 @@ import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
 import {ActiveConnections, Coordinate, IncomingCoordinate} from "./types";
+import mongoose from "mongoose";
+import config from "./config";
+import userRouter from "./routes/user";
 
 const app = express();
+const router = express.Router();
+
 expressWs(app);
 
 const port = 8000;
+app.use(express.json());
 
-app.use(cors());
-
-const router = express.Router();
+app.use(cors({origin: ['http://localhost:5173']}));
+app.use('/users', userRouter);
 
 const activeConnections: ActiveConnections = {};
 let coordinates: Coordinate[] = [];
@@ -56,6 +61,16 @@ router.ws('/paint', (ws) => {
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`server running on ${port} port.`);
-});
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+
+    app.listen(port, () => {
+        console.log(`Server running on ${port} port.`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+void run();
--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,5 +1,18 @@
 import {WebSocket} from 'ws';
 
+export interface UserFields {
+    username: string;
+    password: string;
+    token: string;
+    role: string;
+}
+
+export interface UserMethods {
+    checkPassword(password: string): Promise<boolean>;
+    generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
 export interface ActiveConnections {
     [id: string]: WebSocket
 }
